fc7754703243f28cf3c0f85b6be537fd4ad9c989
shrsyas <112723693+shrsyas@users.noreply.github.com>
2023-11-25 17:48:14 -0600

    added getCountryEnergyUsageInfo for finding energy usage based on country

--- src/geoCarbonUsageMeasurement.js
+++ src/geoCarbonUsageMeasurement.js
@@ -1,4 +1,4 @@
-import { getCarbonUsage, fetchGeoInfo } from './utils/lib.js';
+import { getCarbonUsage, getEnergyUsageInfo, fetchGeoInfo } from './utils/lib.js';
 
 /**
  * Class to fetch carbon usage based on the current location.
@@ -25,6 +25,30 @@ class GeoCarbonUsageMeasurement {
       // Handle any errors that occur during the process
       console.error(`Error fetching carbon usage: ${error.message}`);
     }
+  }
+  
+  /**
+   * Asynchronously retrieves carbon usage information for the current country
+   *
+   * @returns {Promise<object|string>} A promise that resolves to either the country-specific carbon usage information, or a message indicating that no information was found
+   */
+    async getCountryEnergyUsageInfo() {
+
+      try {
+        // fetch the country's geographic information
+        const geoInfo = await fetchGeoInfo();
+        const countryCode3 = geoInfo.country_code3;
+
+        // Retrieve energy usage information for the retrieved country code
+        const energyUsage = await getEnergyUsageInfo(countryCode3);
+
+        // Return the retrieved energy usage information
+        return energyUsage;
+
+      } catch (error) {
+        // Handle any errors that occur during the process
+        console.error(`Error fetching carbon usage: ${error.message}`);
+      }
   }
 }
 
